12b497fb4daf0ad833362504c44f0e85793c5af9
Арамаис <124435679+Aramais1993@users.noreply.github.com>
2023-08-31 03:36:25 +0700

    Add files via upload

---
+++ HW_1/HotBeverage.java
@@ -0,0 +1,22 @@
+import java.util.Calendar;
+
+public class HotBeverage extends Beverage {
+
+    Integer temperature;
+
+    public HotBeverage(String name, int price, int quantity, Calendar bestBefore, double volume, Integer temperature) {
+        super(name, price, quantity, bestBefore, volume);
+        this.temperature = temperature;
+    }
+
+    public Integer getTemp() {
+        return temperature;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", temp=" + temperature;
+
+    }
+
+}
